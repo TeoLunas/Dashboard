0f27e89c71a50c04abd6f8d1d5c1eed96b326226
Teobaldo Lunas <lunasteobaldo@gmail.com>
2022-04-08 01:26:21 -0400

    Se agrega funcionalidad de tema dark y claro

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler')
 
+
+//Mostrar barralateral(menu)
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block'
 })
 
+//Cerrar barralateral(menu)
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none'
+})
+
+//Cambiar tema
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
